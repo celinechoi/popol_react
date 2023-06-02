7a08eb1696f3bca4d5ccadf30506f0d5fb7f3c6c
celinechoi <web.jinseul@gmail.com>
2023-06-02 19:57:06 +0900

    이미지 태그 추가

--- src/Routes/Intro.tsx
+++ src/Routes/Intro.tsx
@@ -1,18 +1,25 @@
 import styled from "styled-components";
 import useCountUp from "../CountUp"
 import {media} from '../style/media_query';
+import good_emoji from "../img/emoji/good.png";
 
 const Visual = styled.div`
 	display: flex;
 	align-items: center;
 	flex-direction: column;
 	justify-content: center;
-	gap: 30px;
 	height: 550px;
 	background-color: ${(props) => props.theme.bgColor.gray.first};
 	${media.medium`
 		height: 440px;
 	`};
+	>img {
+		display: block;
+		width: 267px;
+		${media.medium`
+			width: 23%;
+		`};
+	}
 	>p {
 		font-size: 44px;
 		font-weight: 900;
@@ -29,6 +36,7 @@ function Intro(){
 	return (
 		<>
 			<Visual>
+				<img src={good_emoji} alt="good_emoji" />
 				<p><span>{useCountUp(17)}</span>개의 프로젝트</p>
 				<p><span>{useCountUp(7)}</span>년차</p>
 				<p>UI/UX 개발자 최진슬 입니다.</p>
