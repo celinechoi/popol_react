d37e8f2ca9e73cbe51bc1364852550916a5b32f3
celinechoi <web.jinseul@gmail.com>
2023-06-27 17:38:00 +0900

    데이터 추출 성공!

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -1,25 +1,52 @@
 import { ref, child, get } from "firebase/database";
 import { firebaseDB } from "../firebase";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+interface workInterface {
+	customer:string,
+	endDate:string,
+	id:string,
+	projectName:string,
+	startDate:string
+}
 
 function List(){
-	const readOne = () => {
-		const dbRef = ref(firebaseDB);
-		get(child(dbRef, "/WorkList"))
-			.then(snapshot => {
-			if (snapshot.exists()) {
-				console.log(snapshot.val());
-			} else {
-				console.log("No data available");
-			}
-		})
-			.catch(error => {
-			console.error(error);
-		});
+	let [datas, setData] = useState<workInterface[]>([]);
+	const [loading, setLoading] = useState(true);
+	const readOne = async () => {
+		const snapshot = await get(ref(firebaseDB, '/WorkList/Solution'));
+  	const allDatas = await snapshot.val();
+		setData(allDatas);
+		setLoading(false);
+		console.log(datas);
+		// get(ref(firebaseDB, "/WorkList"))
+		// 	.then(snapshot => {
+		// 	if (snapshot.exists()) {
+		// 		setData(snapshot.val())
+		// 	} else {
+		// 		console.log("No data available");
+		// 	}
+		// })
+		// 	.catch(error => {
+		// 	console.error(error);
+		// });
 	};
 	useEffect(()=>{readOne()}, []);
 	return (
-		<>firebase</>
+		<>
+		{loading ? (
+        "Loading"
+      ) : (
+        <div>
+          {datas.map((data) => (
+            <div key={data.id}>
+              <Link to={`/${data.id}`}>{data.id} &rarr;</Link>
+            </div>
+          ))}
+        </div>
+      )}
+		</>
 	)
 }
 
