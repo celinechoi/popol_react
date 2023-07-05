d7fcf5acb67d9c604762a3b010cab2d00054f2a0
celinechoi <web.jinseul@gmail.com>
2023-07-05 17:51:06 +0900

    CountUp에 useEffect 제거

--- src/function/CountUp.ts
+++ src/function/CountUp.ts
@@ -11,17 +11,15 @@ export default function useCountUp(end: number, start = 0, duration = 2000) {
   const frameRate = 1000 / 60
   const totalFrame = Math.round(duration / frameRate)
 
-  useEffect(() => {
-    let currentNumber = start
-    const counter = setInterval(() => {
-      const progress = easeOutExpo(++currentNumber / totalFrame)
-      setCount(Math.round(end * progress))
+	let currentNumber = start
+	const counter = setInterval(() => {
+		const progress = easeOutExpo(++currentNumber / totalFrame)
+		setCount(Math.round(end * progress))
 
-      if (progress === 1) {
-        clearInterval(counter)
-      }
-    }, frameRate)
-  }, [end, frameRate, start, totalFrame])
+		if (progress === 1) {
+			clearInterval(counter)
+		}
+	}, frameRate)
 
   return count
 }
