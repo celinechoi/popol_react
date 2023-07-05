c08fab2900fbc5fbe4f117d0521964ac3ff86f9c
celinechoi <web.jinseul@gmail.com>
2023-07-05 17:45:16 +0900

    works 텝 스타일 컴포넌트로 수정

--- src/Routes/Works.tsx
+++ src/Routes/Works.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { media } from "../style/media_query";
 import List from "../components/List";
 import { WorkList } from "../api";
-import { Link, Route, Switch, useParams } from "react-router-dom";
+import { Link, Route, Switch, useLocation, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Auth from "./Auth";
 import Si from "components/Si";
@@ -24,41 +24,37 @@ const Title = styled.div`
 			font-size: 44px;
 			font-weight: 700;
 		}
-		.tab {
-			&-list {
-				display: inline-block;
-				clear: both;
-				overflow: hidden;
-				position: relative;
-				top: 40px;
-				padding: 12px;
-				border-radius: 38px;
-				background-color: ${(props) => props.theme.bgColor.gray.first};
-				box-shadow: ${(props) => props.theme.shadow};
-				>li {
-					float: left;
-					margin-left: 12px;
-					padding: 12px 28px;
-					border-radius: 26px;
-					color: ${(props) => props.theme.textColor.gray.fixth};
-					font-size: 18px;
-					font-weight: 900;
-					text-align: center;
-					cursor: pointer;
-					&:hover {
-						background-color: ${(props) => props.theme.point.blue[0]};
-						color: ${(props) => props.theme.textColor.gray.first};
-					}
-					&.on {
-						background-color: ${(props) => props.theme.point.blue[0]};
-						color: ${(props) => props.theme.textColor.gray.first};
-					}
-					&:first-child {
-						margin-left: 0;
-					}
-				}
-			}
-		}
+	}
+`;
+
+const Tabs = styled.ul`
+	display: inline-block;
+	clear: both;
+	overflow: hidden;
+	position: relative;
+	top: 40px;
+	padding: 12px;
+	border-radius: 38px;
+	background-color: ${(props) => props.theme.bgColor.gray.first};
+	box-shadow: ${(props) => props.theme.shadow};
+`;
+
+const Tab = styled.li`
+	float: left;
+	margin-left: 12px;
+	padding: 12px 28px;
+	border-radius: 26px;
+	color: ${(props) => props.theme.textColor.gray.fixth};
+	font-size: 18px;
+	font-weight: 900;
+	text-align: center;
+	cursor: pointer;
+	&:hover {
+		background-color: ${(props) => props.theme.point.blue[0]};
+		color: ${(props) => props.theme.textColor.gray.first};
+	}
+	&:first-child {
+		margin-left: 0;
 	}
 `;
 
@@ -79,24 +75,28 @@ interface CoinInterface {
   id: string;
 }
 
+interface RouteState {
+  name: string;
+}
+
 function Works(){
 	return (
 		<>
 			<Title>
 				<div className="inner">
 					<p>Works</p>
-					<ul className="tab-list">
+					<Tabs>
 						{
 							workType.map((val) => (
-								<li key={val.id}>
+								<Tab key={val.id}>
 									<Link to={{
 										pathname: `/works/${val.id}`,
 										state: {name: val.id}
 									}}>{val.id}</Link>
-								</li>
+								</Tab>
 							))
 						}
-					</ul>
+					</Tabs>
 				</div>
 			</Title>
 		</>
